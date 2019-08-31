3c3b5d36101957599023116d0670a7468a7488af
John Mitsch <jomitsch@redhat.com>
2019-08-31 17:18:08 -0400

    Adding db create

--- entrypoint.rb
+++ entrypoint.rb
@@ -24,8 +24,15 @@ end
 Dir.chdir("#{ROOT_DIR}/foreman") do
   STDOUT.puts "Installing gems"
   `#{RUBY_SCL} bundle install --jobs=5 `
+
   STDOUT.puts "Installing node packages"
   `npm install`
+
+  STDOUT.puts "Setting up db"
+  `#{RUBY_SCL} bundle exec rake db:drop || true`
+  `#{RUBY_SCL} bundle exec rake db:create --trace`
   `#{RUBY_SCL} bundle exec rake db:migrate`
+
+  STDOUT.puts "Starting server"
   `#{RUBY_SCL} bundle exec foreman start`
 end
